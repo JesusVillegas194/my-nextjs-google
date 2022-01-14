91d734d18d8e73163dd77f1f7e2e64e488cfe6fd
JesusVillegas194 <j.villegascambero@gmail.com>
2022-01-14 16:19:53 -0600

    [Add] Main part, logo, input, and buttons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
-import { Microphone, ViewGridIcon } from "@heroicons/react/solid";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -33,6 +35,27 @@ export default function Home() {
       </header>
 
       {/* Body */}
+      <main>
+        <form className="flex flex-col items-center mt-44 flex-grow">
+          <Image
+            src="https://lh3.googleusercontent.com/d_S5gxu_S1P6NR1gXeMthZeBzkrQMHdI5uvXrpn3nfJuXpCjlqhLQKH_hbOxTHxFhp5WugVOEcl4WDrv9rmKBDOMExhKU5KmmLFQVg"
+            height={150}
+            width={300}
+            alt="Google logo"
+          />
+
+          <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+            <SearchIcon className="h-5 mr-3 text-gray-500" />
+            <input type="text" className="flex-grow focus:outline-none" />
+            <MicrophoneIcon className="h-5" />
+          </div>
+
+          <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+            <button className="btn">Google Search</button>
+            <button className="btn">I&apos;m Feeling Lucky</button>
+          </div>
+        </form>
+      </main>
 
       {/* Footer */}
     </div>
